c5543589bfbb509660e9e13f2104af57a0a771fd
Sthefani Laura <slauraic@hotmail.com>
2018-09-20 22:06:50 -0300

    Criar a função da validação de valor

--- script.js
+++ script.js
@@ -5,30 +5,18 @@ let valor = document.querySelector("#valor");
 
 
 botaoEnviar.addEventListener("click", function(evento){
-    
+    evento.preventDefault(); // A função prevent.Default(); impede que as informações de um form sejam apagadas logo após clicar no botão, foi preciso colocá-la em cada uma das condições.
     if(usuario.value === ""){
         alert("Por favor digite seu nome de usuário!");
-        evento.preventDefault(); // A função prevent.Default(); impede que as informações de um form sejam apagadas logo após clicar no botão, foi preciso colocá-la em cada uma das condições.
-
     }
     if(senha.value === ""){
-        alert("Digite a sua senha");
-        evento.preventDefault();
-        
+        alert("Digite a sua senha!");     
     }
     if(senha.value.length < 6){
         alert("Sua senha precisa de pelo menos 6 caracteres!");
-        evento.preventDefault();
-
     }
-    if(valor.value === ""){
+    if(isNaN(valor.value) || valor.value === ""){
         alert("Tem que ser um valor!");
-        evento.preventDefault();
-    }
-    if(valor.value === NaN){
-        alert("Tem que ser um valor!");
-        evento.preventDefault();
-
     }
     else{
         alert("Enviado com sucesso!");
